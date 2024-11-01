44ba6c5ab7a154c14c8e5d0138daac40bb5c2de2
Marvin Albert <marvin.albert@gmail.com>
2024-11-01 07:56:37 +0100

    Fixed convergence check which was missing parenthesis

--- src/multiview_stitcher/registration.py
+++ src/multiview_stitcher/registration.py
@@ -1409,9 +1409,8 @@ def optimize_bead_subgraph(
             break
 
         edges = list(g_beads_subgraph.edges)
-        if (
-            mean_residuals[-1] < abs_tol
-            and mean_residuals[-1] < 1e-6
+        if mean_residuals[-1] < abs_tol and (
+            mean_residuals[-1] < 1e-6
             or (max_residuals[-1] / mean_residuals[-1])
             < max_residual_max_mean_ratio
         ):
