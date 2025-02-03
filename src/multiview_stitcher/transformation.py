55acf87544f3f4841b95e0439df5f89048ab5392
Marvin Albert <marvin.albert@gmail.com>
2025-02-04 00:32:13 +0100

    WIP

--- src/multiview_stitcher/transformation.py
+++ src/multiview_stitcher/transformation.py
@@ -43,6 +43,22 @@ def transform_sim(
     )
     Oy = spatial_image_utils.get_origin_from_sim(sim, asarray=True)
 
+    # take care of floating point errors: slightly enlargen the source support
+    # TODO: This is a hack, we should find a better way to deal with floating
+    # point errors. Could not reproduce the following error in a simple test yet:
+    # In a fractal stitching task: fused output contained empty z slices.
+
+    # if hack:
+    #     Oy -= np.finfo(np.float32).eps
+    #     Sy += np.eye(ndim) * np.finfo(np.float32).eps
+
+    # Oy -= 20 * np.finfo(np.float32).eps
+    # Sy += 20 * np.eye(ndim) * np.finfo(np.float32).eps
+    # Ox += np.finfo(np.float32).eps
+    # Sx -= np.eye(ndim) * np.finfo(np.float32).eps
+    # Oy -= 1e-4
+    # Sy += 1e-4 * np.eye(ndim)
+
     matrix_prime = np.dot(np.linalg.inv(Sy), np.dot(matrix, Sx))
     offset_prime = np.dot(
         np.linalg.inv(Sy),
@@ -53,6 +69,10 @@ def transform_sim(
             Ox,
         ),
     )
+
+    # convert to float
+    # matrix_prime = matrix_prime.astype(np.float64)
+    # offset_prime = offset_prime.astype(np.float64)
 
     affine_transform_kwargs = {
         "matrix": matrix_prime,
