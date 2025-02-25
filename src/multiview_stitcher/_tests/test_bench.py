07dfec53cacf977c7e7945284e8b66054280f278
Marvin Albert <marvin.albert@gmail.com>
2025-02-25 20:17:10 +0100

    Change testing strategy and add dataset

--- src/multiview_stitcher/_tests/test_bench.py
+++ src/multiview_stitcher/_tests/test_bench.py
@@ -23,67 +23,92 @@ test_bench_data_dir = (
 
 datasets = [
     {
-        "name": "tcyx_2x1_0",
-        "image_paths": [test_bench_data_dir / "tcyx_2x1_0/tcyx_2x1_0.czi"],
-        "parameter_path": test_bench_data_dir / "tcyx_2x1_0/tcyx_2x1_0.zarr",
+        "name": "fluo_tcyx_2x1_0",
+        "image_paths": [
+            test_bench_data_dir / "fluo_tcyx_2x1_0/fluo_tcyx_2x1_0.czi"
+        ],
+        "parameter_path": test_bench_data_dir
+        / "fluo_tcyx_2x1_0/fluo_tcyx_2x1_0.zarr",
         "dims": ["t", "c", "y", "x"],
         "visibility": "private",
     },
     {
-        "name": "tcyx_3x3_0_scene0",
-        "image_paths": [test_bench_data_dir / "tcyx_3x3_0/tcyx_3x3_0.czi"],
+        "name": "fluo_tcyx_3x3_0_scene0",
+        "image_paths": [
+            test_bench_data_dir / "fluo_tcyx_3x3_0/fluo_tcyx_3x3_0.czi"
+        ],
         "parameter_path": test_bench_data_dir
-        / "tcyx_3x3_0/tcyx_3x3_0_scene0.zarr",
+        / "fluo_tcyx_3x3_0/fluo_tcyx_3x3_0_scene0.zarr",
         "dims": ["t", "c", "y", "x"],
         "visibility": "private",
         "scene": 0,
     },
     {
-        "name": "tcyx_3x3_0_scene1",
-        "image_paths": [test_bench_data_dir / "tcyx_3x3_0/tcyx_3x3_0.czi"],
+        "name": "fluo_tcyx_3x3_0_scene1",
+        "image_paths": [
+            test_bench_data_dir / "fluo_tcyx_3x3_0/fluo_tcyx_3x3_0.czi"
+        ],
         "parameter_path": test_bench_data_dir
-        / "tcyx_3x3_0/tcyx_3x3_0_scene1.zarr",
+        / "fluo_tcyx_3x3_0/fluo_tcyx_3x3_0_scene1.zarr",
         "dims": ["t", "c", "y", "x"],
         "visibility": "private",
         "scene": 1,
     },
     {
-        "name": "yx_3x3_0",
+        "name": "fluo_yx_3x3_0",
         "image_paths": [
-            test_bench_data_dir / f"yx_3x3_0/tile{i:1d}.zarr" for i in range(9)
+            test_bench_data_dir / f"fluo_yx_3x3_0/tile_{i:1d}.zarr"
+            for i in range(9)
         ],
-        "parameter_path": test_bench_data_dir / "yx_3x3_0/yx_3x3_0.zarr",
-        "dims": ["t", "c", "y", "x"],
+        "parameter_path": test_bench_data_dir
+        / "fluo_yx_3x3_0/fluo_yx_3x3_0.zarr",
+        "dims": ["y", "x"],
         "visibility": "private",
-        "scene": 1,
+        "tolerance": 0.0005,  # pixel spacing is 0.000138
+        "test_edge_pruning_method": "keep_axis_aligned",
         "reg_config": [
             {
-                "reg_binning": {dim: 1 for dim in ["y", "x"]},
+                "registration_binning": {dim: 1 for dim in ["y", "x"]},
             },
             {
-                "reg_binning": {dim: 1 for dim in ["y", "x"]},
+                "registration_binning": {dim: 1 for dim in ["y", "x"]},
                 "pairwise_reg_func": registration.registration_ANTsPy,
                 "pairwise_reg_func_kwargs": {
                     "transform_types": ["Affine"],
                 },
+                "groupwise_resolution_kwargs": {
+                    "transform": "affine",
+                },
             },
         ],
+    },
+    {
+        "name": "em_yx_3x2_0",
+        "modality": "EM",
+        "image_paths": [
+            test_bench_data_dir / f"em_yx_3x2_0/tile_{i:1d}.zarr"
+            for i in range(6)
+        ],
+        "parameter_path": test_bench_data_dir / "em_yx_3x2_0/em_yx_3x2_0.zarr",
+        "dims": ["y", "x"],
+        "tolerance": 1000000,  # pixel spacing is 0.02 (um)
+        "visibility": "private",
     },
 ]
 
 
-def read_params(dataset):
-    xparamss = xr.open_zarr(dataset["parameter_path"]).compute()
+def read_params(filename):
+    xparamss = xr.open_zarr(filename).compute()
 
     return [xparamss[str(ind)] for ind in range(len(xparamss))]
 
 
-def write_params(params, dataset):
+def write_params(params, filename):
     xparams = xr.Dataset(
         {str(ind): param for ind, param in enumerate(params)},
     )
 
-    xparams.to_zarr(dataset["parameter_path"])
+    xparams.to_zarr(filename)
 
 
 def get_msims_from_dataset(dataset):
@@ -124,7 +149,7 @@ def register_dataset(msims, dataset):
     for irun, reg_run_config in enumerate(reg_run_configs):
         output_transform_key = "reg_" + str(irun)
 
-        params = registration.register(
+        registration.register(
             msims,
             transform_key=input_transform_key,
             new_transform_key=output_transform_key,
@@ -133,30 +158,44 @@ def register_dataset(msims, dataset):
 
         input_transform_key = output_transform_key
 
+    params = [
+        si_utils.get_affine_from_sim(
+            msi_utils.get_sim_from_msim(msim),
+            transform_key=output_transform_key,
+        )
+        for msim in msims
+    ]
+
+    # from multiview_stitcher import vis_utils
+    # vis_utils.view_neuroglancer(
+    #     sims=[msi_utils.get_sim_from_msim(msim) for msim in msims],
+    #     zarr_paths=dataset["image_paths"],
+    #     channel_coord=0,
+    #     transform_key=output_transform_key,
+    # )
+
     return params
 
 
 @pytest.mark.private_data
 @pytest.mark.parametrize(
     "dataset",
-    [
-        ds
-        for ds in datasets
-        if ds["visibility"] == "private" and "yx_3x3_0" in ds["name"]
-    ],
+    list(datasets),
 )
-def test_pairwise_reg_against_sample_gt(dataset):
+def test_reg_against_reference_params(dataset):
     """
 
     Compare registered parameters with the reference parameters.
 
-    Idea for parameter comparison:
+    Strategy for parameter comparison:
     - for each overlapping pair (according to metadata transformation):
-      - calculate overlap bbox vertices in metadata transformation, pts1, pts2
-      - ref reg Tr1, : project pts1, project pts2
-      - new reg Tn1: project pts1, project pts2
-      - make sure Tr2(pts2) - Tr1(pts1) ~= Tn2(pts2) - Tn1(pts1)
-    - make sure this is true for all overlapping pairs
+      - get points flanking the overlap bbox in metadata world coords
+      - transform these into world coords using the ref params of the first ipairel
+      - assume this coord cooincides for the pair
+      - back project the vertices using ref params
+      - transform to world coords of params to be tested
+      - ideally, the coordinates should coincide again
+      - check that distance between them is smaller than tolerance
 
     Parameters
     ----------
@@ -165,30 +204,58 @@ def test_pairwise_reg_against_sample_gt(dataset):
 
     msims = get_msims_from_dataset(dataset)
 
-    params_ref = read_params(dataset)
+    spacing = si_utils.get_spacing_from_sim(
+        msi_utils.get_sim_from_msim(msims[0])
+    )
+
+    sdims = si_utils.get_spatial_dims_from_sim(
+        msi_utils.get_sim_from_msim(msims[0])
+    )
+
+    if "tolerance" in dataset:
+        tolerance = dataset["tolerance"]
+    else:
+        tolerance = max([spacing[dim] for dim in sdims])
+
+    params_ref = read_params(dataset["parameter_path"])
     params_reg = register_dataset(msims, dataset)
-    # params_reg = params_ref# + np.random.random() / 100.
 
     g_adj = mv_graph.build_view_adjacency_graph_from_msims(
         msims,
         transform_key=METADATA_TRANSFORM_KEY,
     )
 
-    pairs = list(g_adj.edges())
-    sdims = si_utils.get_spatial_dims_from_sim(
-        msi_utils.get_sim_from_msim(msims[0])
-    )
+    if "test_edge_pruning_method" in dataset:
+        g_adj = registration.prune_view_adjacency_graph(
+            g_adj,
+            method=dataset["test_edge_pruning_method"],
+        )
 
-    spacing = si_utils.get_spacing_from_sim(
-        msi_utils.get_sim_from_msim(msims[0])
-    )
-    tolerance = max([spacing[dim] for dim in sdims])
+    pairs = list(g_adj.edges())
 
     # compare the registered parameters with the reference
-    for ipair, pair in enumerate(pairs):
+    for _ipair, pair in enumerate(pairs):
+        msim0 = msi_utils.multiscale_sel_coords(
+            msims[pair[0]],
+            {
+                "t": msi_utils.get_sim_from_msim(msims[pair[0]])
+                .coords["t"]
+                .values[0]
+            },
+        )
+
+        msim1 = msi_utils.multiscale_sel_coords(
+            msims[pair[1]],
+            {
+                "t": msi_utils.get_sim_from_msim(msims[pair[1]])
+                .coords["t"]
+                .values[0]
+            },
+        )
+
         lower, upper = registration.get_overlap_bboxes(
-            msi_utils.get_sim_from_msim(msims[pair[0]]),
-            msi_utils.get_sim_from_msim(msims[pair[1]]),
+            msi_utils.get_sim_from_msim(msim0),
+            msi_utils.get_sim_from_msim(msim1),
             input_transform_key=METADATA_TRANSFORM_KEY,
             output_transform_key=None,
         )
@@ -207,16 +274,44 @@ def test_pairwise_reg_against_sample_gt(dataset):
         )
 
         for t in params_ref[0].coords["t"].values:
-            param_displacements = []
-            for _iparam, params in enumerate([params_reg, params_ref]):
-                vertices_in_world_coords = [
-                    transformation.transform_pts(vs, p.sel(t=t))
-                    for p in [params[pair[0]], params[pair[1]]]
-                ]
-                param_displacements.append(
-                    vertices_in_world_coords[1] - vertices_in_world_coords[0]
+            # transform vertices using the reference param of the first ipairel
+            vs_ref_world = transformation.transform_pts(
+                vs, params_ref[pair[0]].sel(t=t)
+            )
+
+            # assume this coord cooincides for the pair and
+            # - back project the vertices using ref params
+            # - re project using reg params
+            vs_ref_world_ref_data_reg_worlds = []
+            for ipairel in [0, 1]:
+                vs_ref_world_ref_data = transformation.transform_pts(
+                    vs_ref_world,
+                    np.linalg.inv(params_ref[pair[ipairel]].sel(t=t)),
                 )
 
-            assert np.allclose(
-                param_displacements[0], param_displacements[1], atol=tolerance
-            ), f"dataset {dataset}, pair {ipair}, t={t}, {param_displacements[0]} != {param_displacements[1]}"
+                vs_ref_world_ref_data_reg_world = transformation.transform_pts(
+                    vs_ref_world_ref_data, params_reg[pair[ipairel]].sel(t=t)
+                )
+
+                vs_ref_world_ref_data_reg_worlds.append(
+                    vs_ref_world_ref_data_reg_world
+                )
+
+            # from matplotlib import pyplot as plt
+            # plt.figure()
+            # plt.scatter(*vs_ref_world.T, c='black')
+            # plt.scatter(*vs_ref_world_ref_data_reg_worlds[0].T, c='r')
+            # plt.scatter(*vs_ref_world_ref_data_reg_worlds[1].T, c='g')
+            # plt.show()
+
+            # check that the coordinates coincide again
+            assert (
+                np.max(
+                    np.linalg.norm(
+                        vs_ref_world_ref_data_reg_worlds[1]
+                        - vs_ref_world_ref_data_reg_worlds[0],
+                        axis=-1,
+                    )
+                )
+                < tolerance
+            )
