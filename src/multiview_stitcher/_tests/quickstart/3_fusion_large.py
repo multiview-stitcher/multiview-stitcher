a5f76cb97e06f1c69e3a0cbc9dc18466382372cc
Marvin Albert <marvin.albert@gmail.com>
2025-10-26 23:25:03 +0100

    Integrate and move fuse_to_zarr and fuse_to_ome_zarr into fusion.fuse API

--- src/multiview_stitcher/_tests/quickstart/3_fusion_large.py
+++ src/multiview_stitcher/_tests/quickstart/3_fusion_large.py
@@ -1,18 +1,20 @@
 from multiview_stitcher import fusion, misc_utils
 
-output_zarr_url = "fused_output.ome.zarr"
-
-fused = fusion.fuse_to_multiscale_ome_zarr(
-    fuse_kwargs={
-        "sims": [msi_utils.get_sim_from_msim(msim) for msim in msims],
-        "transform_key": "translation_registered",
-        # ... further optional args for fusion.fuse
+fused = fusion.fuse(
+    sims=[msi_utils.get_sim_from_msim(msim) for msim in msims],
+    transform_key="translation_registered",
+    # ... further optional args for fusion.fuse
+    output_zarr_url="fused_output.ome.zarr",
+    zarr_options={
+        "ome_zarr": True,
+        # "ngff_version": "0.4",  # optional
     },
-    output_zarr_url=output_zarr_url,
     # optionally, we can use ray for parallelization (`pip install "ray[default]"`)
-    # batch_func=misc_utils.process_batch_using_ray,
-    # n_batch=4, # number of chunk fusions to schedule / submit at a time
-    # batch_func_kwargs={
-    #     'num_cpus': 4 # number of processes for parallel processing to use with ray
+    # batch_options={
+    #     "batch_func": misc_utils.process_batch_using_ray,
+    #     "n_batch": 4,  # number of chunk fusions to schedule / submit at a time
+    #     "batch_func_kwargs": {
+    #         'num_cpus': 4  # number of processes for parallel processing to use with ray
     #     },
+    # },
 )
